b1798f594ca162e3def3caee19e68ef31aaf38c7
Dennis Bunfield <dennis@factoid.org>
2020-10-26 17:42:30 -0500

    updated to use tendermint db

--- ValAcc/database/database.go
+++ ValAcc/database/database.go
@@ -14,7 +14,7 @@ package database
 // see ValAcc/types/types.go for the constants for bucket names
 
 import (
-	"fmt"
+	//"fmt"
 	//"os"
 
 	"github.com/AccumulusNetwork/ValidatorAccumulator/ValAcc/types"
@@ -24,7 +24,7 @@ import (
 type DB struct {
 	DBHome   string
 	//badgerDB *badger.DB
-	db2 *dbm.DB
+	db2 dbm.DB
 }
 
 
@@ -32,7 +32,7 @@ type DB struct {
 // We take an instance of the database, because we anticipate sometime in the future,
 // running multiple instances of the database.  This feature might not ever be used
 // for the ValAcc project, but it has been useful for factomd testing.
-func (d *DB) InitDB(db *dbm.DB) {
+func (d *DB) InitDB(db dbm.DB) {
     d.db2 = db
 }
 
@@ -71,9 +71,14 @@ func GetKey(bucket string, key []byte) (CKey []byte) {
 // is found for the given key
 func (d *DB) Get(bucket string, key []byte) (value []byte) {
 	//d.db2.Get()
-	/*
-	CKey := GetKey(bucket, key) // combine the bucket and the key
 
+
+	CKey := GetKey(bucket, key) // combine the bucket and the key
+	value, err := d.db2.Get(CKey)
+	if err != nil {
+		return nil
+	}
+	/*
 	// Go look up the CKey, and return any error we might find.
 	err := d.badgerDB.View(func(txn *badger.Txn) error {
 		item, err := txn.Get(CKey)
@@ -105,14 +110,17 @@ func (d *DB) GetInt32(bucket string, ikey uint32) (value []byte) {
 // Put a key/value in the database.  We return an error if there was a problem
 // writing the key/value pair to the database.
 func (d *DB) Put(bucket string, key []byte, value []byte) error {
-	/*CKey := GetKey(bucket, key)
+	CKey := GetKey(bucket, key)
+
+	err := d.db2.Set(CKey,value)
+	/*
 
 	// Update the key/value in the database
 	err := d.badgerDB.Update(func(txn *badger.Txn) error {
 		err := txn.Set([]byte(CKey), value)
 		return err
 	}) */
-	err := fmt.Errorf("barf %x", 1.0);
+
 	return err
 }
 
